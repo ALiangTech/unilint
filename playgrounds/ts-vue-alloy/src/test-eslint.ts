3d24fb0640fd1e76193b94ab28950a86d454aea4
AliangTech <1305544590@qq.com>
2023-06-20 13:58:27 +0800

    测试文件

---
+++ playgrounds/ts-vue-alloy/src/test-eslint.ts
@@ -0,0 +1,21 @@
+class Foo2 {
+    static foo = 'foo';
+    static getFoo() {
+      return Foo2.foo;
+    }
+    constructor() {}
+    bar = 'bar';
+    getBar() {}
+    get baz() {
+      return 'baz';
+    }
+    set baz(value) {
+      console.log(value);
+    }
+  }
+
+
+  enum E1 {
+    A = 0,
+    B = 0,
+  }
